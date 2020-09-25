bb0ff4f1529b43c72ff33c096808cbacf13357c7
TEOREMA Studio <teoremastudio@MacBook-Pro-de-TEOREMA.local>
2020-09-25 04:59:13 -0500

    Start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/assets/css/style.css';
+import './assets/css/style.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
